783cbefd3634caee5c63d367b161b0befb627552
minhtv.work@gmail.com <minhtv.work@gmail.com>
2023-12-28 15:07:43 +0700

    PT Shop UI

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AutocompleteComponent } from './component/autocomplete/autocomplete.component';
+import { InputComponent } from './input/input.component';
+import { HomeComponent } from './component/home/home.component';
+import { CardComponent } from './component/card/card.component';
+import { TableComponent } from './component/table/table.component';
+import { FormdesignComponent } from './component/formdesign/formdesign.component';
+import { AssociateComponent } from './component/associate/associate.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'autocomplete',component:AutocompleteComponent},
+  {path:'input',component:InputComponent},
+  {path:'card',component:CardComponent},
+  {path:'table',component:TableComponent},
+  {path:'form',component:FormdesignComponent},
+  {path:'associate',component:AssociateComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
